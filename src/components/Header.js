df6283af7a656b84ccaaef45741f8dbcd02f5164
David ZAGORSKI <davidzagorski@Davids-MacBook-Air.local>
2021-02-18 15:00:50 +0200

    From terminal

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="http://www.logo-designer.co/wp-content/uploads/2013/06/Qatar-Jet-Fuel-Company-logo-design-branding-Sandor-Szalay-525.jpg" />
+    </header>
+  );
+};
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,19 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+      </div>
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New Posts</div>
+      </div>
+      <div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
+    </div>
+  );
+};
+export default Profile;
