ac2f2657872407ebabf9822625bf98dabfa34f46
davidzagorski <daavidzzz@gmail.com>
2021-02-19 17:43:10 +0200

    NavLinks added. Active links css added. NavLink route's added.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,19 +5,19 @@ const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <NavLink to="/profile">Profile</NavLink>
+                <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <NavLink to="/dialogues">Messages</NavLink>
+                <NavLink to="/dialogues" activeClassName={s.activeLink}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="">News</NavLink>
+                <NavLink to="/test2"activeClassName={s.activeLink}>News</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="">Music</NavLink>
+                <NavLink to="/test3"activeClassName={s.activeLink}>Music</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/test">Settings</NavLink>
+                <NavLink to="/test"activeClassName={s.activeLink}>Settings</NavLink>
             </div>
         </nav>
     );
