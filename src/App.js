141f419b4b8a2905e4b9629c70096564d1774f4f
davidzagorski <daavidzzz@gmail.com>
2021-02-19 17:20:26 +0200

    IDE commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Profile from "./components/Profile";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Dialogues from "./components/Dialogues/Dialogues";
+import Profile from "./components/Profile/Profile";
+import {BrowserRouter, Route} from 'react-router-dom';
 
 const App = () => {
-  return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path="/dialogues" component={Dialogues}/>
+                    <Route path="/profile" component={Profile}/>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 };
 
 export default App;
---
+++ src/components/Dialogues/Dialogues.js
@@ -0,0 +1,11 @@
+import s from './Dialogues.module.css'
+
+const Dialogues = (props) => {
+    return (
+        <div>
+            Dialogues
+        </div>
+    );
+}
+
+export default Dialogues;
