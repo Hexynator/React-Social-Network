d52615391dd9f8a711a4b67db7dc63c1fe127208
davidzagorski <daavidzzz@gmail.com>
2021-03-13 17:35:15 +0200

    Issue with status on 73. Need to fix after API limit refreshed.

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -15,7 +15,7 @@ const ProfileInfo = (props) => {
             {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.small}/>
-                <ProfileStatus status={"Hello my friend"}/>
+                <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
             </div>
         </div>
     );
